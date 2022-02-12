4a7eb2d7569c54fa71b32644bd2e7804c9144f89
Christian Rodriguez <Christianr237@gmail.com>
2022-02-12 03:26:23 -0700

    first generatePassword setup

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
+function generatePassword() {
+  var length ;
+  if (lengthPrompt === 8) {
+    length = 8;
+  } else {
+    length = 12;
+  }
 
+  var lowercase ;
+  if (lowercasePrompt === yes) {
+    charset = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  var numeric ;
+  if (numericPrompt === yes) {
+    numeric = "1234567890";
+  } else {
+    numeric = "";
+  }
+
+  var special ;
+  if (specialPrompt === yes) {
+    charset = "~`!@#$%^&*()_-+=[{]}|;:',.></?";
+  } else {
+    charset = "";
+  }
+
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +43,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
